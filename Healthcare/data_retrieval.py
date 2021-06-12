3f6a5fe0205d8b43296da4bc5b82af47bcbbd5fd
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-12 16:14:34 -0400

    python read file

---
+++ Healthcare/data_retrieval.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def get_data():
+    df_value = pd.read_csv("/Files/clean_value_df.csv", nrows=100)
+    df_census = pd.read_csv("/Files/final_census_df.csv", nrows=100)
+    df_readmission = pd.read_csv("/Files/readmissions_df.csv", nrows=100)
+    df_zipcode = pd.read_csv("/Files/zipcode_lat_lng.csv", nrows=100)
+    return df_value.head(50), df_census.head(50), df_readmission.head(50), df_zipcode.head(50)
+

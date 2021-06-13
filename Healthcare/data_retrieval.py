1c8f2be2ee5df117f1aa594f35ec63711506da88
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-13 12:09:58 -0400

    merged

--- Healthcare/data_retrieval.py
+++ Healthcare/data_retrieval.py
@@ -4,7 +4,14 @@ def get_data():
     df_value = pd.read_csv("../Files/clean_value_df.csv", nrows=100)
     df_census = pd.read_csv("../Files/final_census_df.csv", nrows=100)
     df_readmission = pd.read_csv("../Files/readmissions_df.csv", nrows=100)
-    df_zipcode = pd.read_csv("../Files/zipcode_lat_lng.csv", nrows=100)
     return df_value.head(50), df_census.head(50), df_readmission.head(50), df_zipcode.head(50)
 
-    
+
+def merge_data(df_value, df_census, df_readmission):
+    df_value['zip_code'] = df_value['zip_code'].astype(str)
+    df_census['zip_code'] = df_census['zip_code'].astype(str)
+    merged_df=df_value.merge(df_readmission, how="inner", on="facility_id")
+    print(merged_df.columns)
+    census_merged_df=df_value.merge(df_census, how="inner", on="zip_code")
+    print(census_merged_df.columns)
+    return merged_df, df_census, census_merged_df
--- Healthcare/execute.py
+++ Healthcare/execute.py
@@ -1,4 +1,7 @@
-from data_retrieval import get_data
+from data_retrieval import get_data, merge_data
 
-df_value, df_census, df_readmission, df_zipcode = get_data()
+df_value, df_census, df_readmission = get_data()
 print(df_value[:5], df_census[:5], df_readmission[:5], df_zipcode[:5])
+
+merged_df, census_merged_df=merge_data(df_value, df_census, df_readmission)
+print(merged_df[:5], census_merged_df[:5])

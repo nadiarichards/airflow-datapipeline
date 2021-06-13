bc7d7cedae74777540365a2412e5dc9d80739b1f
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-13 11:56:13 -0400

    updated python

--- Healthcare/data_retrieval.py
+++ Healthcare/data_retrieval.py
@@ -7,6 +7,4 @@ def get_data():
     df_zipcode = pd.read_csv("../Files/zipcode_lat_lng.csv", nrows=100)
     return df_value.head(50), df_census.head(50), df_readmission.head(50), df_zipcode.head(50)
 
-# print(get_data())
-# df_value, df_census, df_readmission, df_zipcode = get_data()
-# print(df_value[:5], df_census[:5], df_readmission[:5], df_zipcode[:5])
+    

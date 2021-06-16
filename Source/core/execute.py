b70cd36fdc26707abdaa2a1494d80f0503dd6e37
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-16 06:57:21 -0400

    dags update

--- Source/core/execute.py
+++ Source/core/execute.py
@@ -18,19 +18,10 @@ def core_aggregate_data(**kwargs):
     task_instance.xcom_push(key='avg_facility_readmission', value=avg_facility_readmission)
     task_instance.xcom_push(key='updated_census_df', value=updated_census_df)
     
-
-
-    updated_readmission_df=merged_df[["facility_id", "city", "zip_code", "payment", "value_code", "measure_name", "number_of_discharges", "excess_readmission_ratio", "number_of_readmissions"]]
-    avg_code_readmission=updated_readmission_df.groupby(["facility_id"]).mean()
-    avg_facility_readmission=updated_readmission_df.groupby(["value_code"]).mean()
-    updated_census_df=census_merged_df[["facility_id", "city", "zip_code", "value_code", "population", "poverty_rate", "median_income"]]
-    return avg_code_readmission, avg_facility_readmission, updated_census_df
-
-
-avg_code_readmission, avg_facility_readmission, updated_census_df=aggregate_data(merged_df, census_merged_df)
-print(avg_code_readmission.head(), avg_facility_readmission.head(), updated_census_df.head())
-
-engine=create_engine_pgsql()
-insert_to_db(engine, avg_code_readmission, 'avg_readmissions')
-insert_to_db(engine, avg_facility_readmission, 'avg_valuecode_readmissions')
-insert_to_db(engine, updated_census_df, 'hospitals_census')
+def core_insert_to_db(**kwargs):
+    task_instance = kwargs['ti']
+    engine=create_engine_pgsql()
+    avg_code_readmission=task_instance.xcom_pull(key='avg_code_readmission', task_ids='aggregate_data')
+    avg_facility_readmission=task_instance.xcom_pull(key='avg_facility_readmission', task_ids='aggregate_data')
+    updated_census_df=task_instance.xcom_pull(key='updated_census_df', task_ids='aggregate_data')
+    insert_to_db(engine,df)

a089a98e0cb3ecdf4969f7d5cc8075d6959cf39c
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-16 06:47:50 -0400

    updated dags

--- Source/core/execute.py
+++ Source/core/execute.py
@@ -2,25 +2,35 @@ from Healthcare.data_retrieval import get_data, merge_data
 from Healthcare.aggregation import aggregate_data
 from Healthcare.connect import create_engine_pgsql, insert_to_db
 
-def get_data():
-    df_value = pd.read_csv("../Files/clean_value_df.csv")
-    df_census = pd.read_csv("../Files/final_census_df.csv")
-    df_readmission = pd.read_csv("../Files/readmissions_df.csv")
-    return df_value, df_census, df_readmission
+def core_get_data(**kwargs):
+    task_instance =kwargs['ti']
+    df_value, df_census, df_readmission = get_data()
+    merged_df, census_merged_df=merge_data(df_value, df_census, df_readmission)
+    task_instance.xcom_push(key='merged_df', value=merged_df)
+    task_instance.xcom_push(key='census_merged_df', value=census_merged_df)
+
+def core_aggregate_data(**kwargs):
+    task_instance = kwargs['ti']
+    merged_df = task_instance.xcom_pull(key='merged_df', task_ids ='get_data')
+    census_merged_df = task_instance.xcom_pull(key='census_merged_df', task_ids ='get_data')
+    avg_code_readmission, avg_facility_readmission, updated_census_df=aggregate_data(merged_df, census_merged_df)
+    task_instance.xcom_push(key='avg_code_readmission', value=avg_code_readmission)
+    task_instance.xcom_push(key='avg_facility_readmission', value=avg_facility_readmission)
+    task_instance.xcom_push(key='updated_census_df', value=updated_census_df)
+    
 
 
-def merge_data(df_value, df_census, df_readmission):
-    df_value['zip_code'] = df_value['zip_code'].astype(str)
-    df_census['zip_code'] = df_census['zip_code'].astype(str)
-    merged_df=df_value.merge(df_readmission, how="inner", on="facility_id")
-    print(merged_df.columns)
-    census_merged_df=df_value.merge(df_census, how="inner", on="zip_code")
-    print(census_merged_df.columns)
-    return merged_df, census_merged_df
-
-    def aggregate_data(merged_df, census_merged_df):
     updated_readmission_df=merged_df[["facility_id", "city", "zip_code", "payment", "value_code", "measure_name", "number_of_discharges", "excess_readmission_ratio", "number_of_readmissions"]]
     avg_code_readmission=updated_readmission_df.groupby(["facility_id"]).mean()
     avg_facility_readmission=updated_readmission_df.groupby(["value_code"]).mean()
     updated_census_df=census_merged_df[["facility_id", "city", "zip_code", "value_code", "population", "poverty_rate", "median_income"]]
     return avg_code_readmission, avg_facility_readmission, updated_census_df
+
+
+avg_code_readmission, avg_facility_readmission, updated_census_df=aggregate_data(merged_df, census_merged_df)
+print(avg_code_readmission.head(), avg_facility_readmission.head(), updated_census_df.head())
+
+engine=create_engine_pgsql()
+insert_to_db(engine, avg_code_readmission, 'avg_readmissions')
+insert_to_db(engine, avg_facility_readmission, 'avg_valuecode_readmissions')
+insert_to_db(engine, updated_census_df, 'hospitals_census')
